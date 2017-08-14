63ff7bc17cae46995dced4d64207df5869284b9f
Davo Hynds <dbhynds@gmail.com>
2017-08-13 19:52:05 -0500

    ichimoku strategy

---
+++ strategies/ichimoku.py
@@ -0,0 +1,128 @@
+import numpy
+from scipy import stats
+import strategy
+import matplotlib.pyplot as plt
+import pandas as pd
+
+class Ichimoku(strategy.Strategy):
+
+    def __init__(self, data):
+        self.data = data
+        self.setExchangeFee(0)
+        self.reset()
+
+    def executeStrategy(self, test_data):
+        last_index = test_data.iloc[-1].name
+        # print last_index
+
+        params = [9, 26, 52, 22]   # [convert_window, base_window, span_B_window, lagging_shift]
+        ichimoku = self.findIchimoku(test_data, *params)
+        # self.plot(ichimoku, test_data)
+
+        test_frame = ichimoku.loc[last_index]
+
+        conversion = test_frame['conversion']
+        base = test_frame['base']
+        span_a = test_frame['span_a']
+        span_b = test_frame['span_b']
+
+        # True = green cloud, False = red cloud
+        cloud_trend = span_a > span_b
+        # Tenkan is above the Kijun
+        line_trend = conversion > base
+        # Tenkan is above the cloud
+        line_cloud_trend = conversion > span_a and conversion > span_b
+
+        # Either Tenkan is above the Kijun with a green cloud, or the cloud is red but we've already bought
+        buy = (line_trend and cloud_trend and line_cloud_trend) or (line_trend and line_cloud_trend and self.bought)
+
+        # print buy
+        if buy: print last_index
+
+        return buy
+
+    def plot(self, ichimoku, test_data):
+
+        column_names = list(ichimoku.columns)
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111, ylabel='ETH Price in BTC')
+
+        colors = ['blue', 'cyan', 'red', 'green']
+        for col, color in zip(column_names, colors):
+            if col not in ['lagging_close', 'timestamp']:
+                ax1.plot(ichimoku.index, ichimoku[col], label=col, color=color)
+
+        ax1.plot(test_data.index, test_data.close, linestyle=':', label="Close Price", color='k')
+
+        # Fill the clouds
+
+        a = ichimoku['span_a']
+        b = ichimoku['span_b']
+
+        plt.fill_between(ichimoku.index, a, b, where=a >= b, facecolor='green', alpha='0.2', interpolate=True)
+        plt.fill_between(ichimoku.index, a, b, where=b >= a, facecolor='red', alpha='0.2', interpolate=True)
+
+        ax1.legend(bbox_to_anchor=(1.5, 1.05))
+        plt.xticks(rotation='vertical')
+
+        print ichimoku.describe()
+        plt.show()
+
+    # Conversion Line
+
+    def findRollingHighsAndLowsLine(self, high_data, low_data, window):
+        '''
+        Can be used to find conversion line or base line depending on the window size
+        
+        Inputs:
+        high_data: pandas column from dataframe
+        low_data: pandas column from dataframe
+        window: int, window size
+        
+        Returns:
+        line: 
+        '''
+        
+        high = high_data.rolling(window=window).max()
+        low = low_data.rolling(window=window).min()
+        line = (high + low)/2
+
+        return line.to_frame()
+
+    def findSpanA(self, conversion, base):
+        span_a = (conversion['conversion'] + base['base'])/2
+
+        return span_a.to_frame()
+
+    def findIchimoku(self, data, convert_window, base_window, span_b_window, lagging_shift):
+        high_data = data['high']
+        low_data = data['low']
+        timestamp = data['timestamp']
+        
+        conversion = self.findRollingHighsAndLowsLine(high_data, low_data, convert_window)
+        conversion = conversion.rename(columns={0:'conversion'})
+
+        base = self.findRollingHighsAndLowsLine(high_data, low_data, base_window)
+        base = base.rename(columns={0:'base'})
+
+        span_b = self.findRollingHighsAndLowsLine(high_data, low_data, span_b_window)
+        span_b = span_b.rename(columns={0:'span_b'})
+
+        span_a = self.findSpanA(conversion, base)
+        span_a = span_a.rename(columns={0:'span_a'})
+
+        lagging = data['close'].shift(-lagging_shift).to_frame()
+        lagging = lagging.rename(columns={'close':'lagging_close'})
+
+        measures = [timestamp, conversion, base, span_b, span_a, lagging]
+
+        ichimoku = pd.concat(measures, axis=1)
+
+        ichimoku.index = pd.to_datetime(ichimoku['timestamp'], unit='s')
+        del ichimoku.index.name
+
+        return ichimoku
+
+
+
+
--- tinker.py
+++ tinker.py
@@ -11,8 +11,7 @@ import requests
 
 import gather_data
 import exchange_rate
-import strategies.momentum as momentum
-import strategies.spike as spike
+import strategies.ichimoku as ichimoku
 import strategy_test
 import trade
 import candlestick
@@ -23,11 +22,6 @@ dotenv.load_dotenv(join(dirname(__file__), '.env'))
 
 # btceth.scheduleRatePulls(interval=PULL_RESOLUTION)
 # time.sleep(1)
-
-btceth = gather_data.GatherData('BTC', 'ETH')
-
-data = btceth.retrieveExchangeRates();
-
 # results = []
 
 # for i in data2:
@@ -38,76 +32,39 @@ data = btceth.retrieveExchangeRates();
 
 # https://poloniex.com/public?command=returnChartData&currencyPair=BTC_ETH&start=1501987800&end=1502107800&period=300
 
-url = 'https://poloniex.com/public?command=returnChartData&currencyPair=BTC_ETH&start=1501987800&end=1502107800&period=300'
-response = requests.get(url)
+candle = candlestick.Candlestick()
+data = candle.fetchAll('ETH', 'BTC')
+data['rate'] = data['high'] + data['low'] / 2
 
-plx_btceth = candlestick.Candlestick()
+print data.describe()
 
-for candle in response.json():
-    plx_btceth.timestamp = candle['date']
-    plx_btceth.currency_from = 'ETH'
-    plx_btceth.currency_to = 'BTC'
-    plx_btceth.high = candle['high']
-    plx_btceth.low = candle['low']
-    plx_btceth.open = candle['open']
-    plx_btceth.close = candle['close']
-    plx_btceth.volume = candle['volume']
-    plx_btceth.quote_volume = candle['quoteVolume']
-    plx_btceth.weighted_average = candle['weightedAverage']
-    plx_btceth.create()
+# data1 = data[:875]
+# data2 = data[876:]
 
+cloud = ichimoku.Ichimoku(data)
+cloud.setExchangeFee(.11)
 
-data1 = data[:875]
-data2 = data[876:]
-
-spike = spike.Spike(data1)
-momentum = momentum.Momentum(data2)
-spike.setExchangeFee(.11)
-momentum.setExchangeFee(.11)
+# cloud.executeStrategy(data)
 
 # # Strategies
 
-def runSpike(spike):
+def runCloud(cloud):
 
-    spike.setTradeThreshold(2200)
-    spike.setInitialInvestment(1000)
-    spike.setResolution(50)
-    spike.run()
-    spike.report()
+    # cloud.setTradeThreshold(2200)
+    cloud.setInitialInvestment(1000)
+    cloud.setResolution(52)
+    cloud.run()
+    cloud.report()
 
-def runMomentum(momemntum):
-
-    momentum.setTradeThreshold(3250)
-    momentum.setInitialInvestment(1000)
-    momentum.setResolution(225)
-    momentum.run()
-    momentum.report()
-
-# runSpike(spike)
-# runMomentum(momentum)
-
-
-
+runCloud(cloud)
 
 # # Run tests
 
-def test_spike(spike):
+def test_cloud(cloud):
 
-    spike_test = strategy_test.StrategyTest(spike)
-    spike_test.setRange(1000, 6000)
-    spike_test.setInterval(100)
-    spike_test.testThreshold(resolution = 50)
-    spike_test.report()
-
-def test_momentum(momentum):
-
-    momentum_test = strategy_test.StrategyTest(momentum)
-    momentum_test.setRange(150, 300)
-    momentum_test.setInterval(50)
-    momentum_test.testResolution(threshold = 3250)
-    momentum_test.report()
-
-# test_spike(spike)
-# test_momentum(momemntum)
-
+    cloud_test = strategy_test.StrategyTest(cloud)
+    cloud_test.setRange(1000, 6000)
+    cloud_test.setInterval(100)
+    cloud_test.testThreshold(resolution = 50)
+    cloud_test.report()
 
